251d530ccb6ed1d9b817d056bf61d9cfe8c06b33
YASHBOMBLE <yashbomble2002@gmail.com>
2023-07-07 07:21:40 -0700

    user schema created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 app.use(express.json());
+
+
+
+
 app.listen(5000,()=>{
     console.log('Server is running on port 5000');
 })
